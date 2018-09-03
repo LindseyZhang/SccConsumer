8d85372d410ebd4cb9cc52565f5022b400ac69f0
lindseyzhang <lindseyzlp@hotmail.com>
2018-09-03 15:10:50 +0800

    spring cloud contract consumer example

---
+++ src/main/java/com/thoughtworks/SccConsumer/SccConsumerApplication.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.SccConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+@SpringBootApplication
+public class SccConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccConsumerApplication.class, args);
+	}
+
+	@Bean
+	RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory("http://localhost:8080"));
+		return restTemplate;
+	}
+}
---
+++ src/test/java/com/thoughtworks/SccConsumer/contracts/TestUser.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.SccConsumer.contracts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureStubRunner(ids = {"com.thoughtworks:SccProducer:+:stubs:8080"}, stubsMode = StubRunnerProperties.StubsMode.LOCAL)
+public class TestUser {
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	@Test
+	public void test_get_users() {
+		final String users = restTemplate.getForObject("/users", String.class);
+		final String expectedUsers = "[{\"username\":\"Zhang san\"}]";
+		assertThat(users).isEqualTo(expectedUsers);
+	}
+}
